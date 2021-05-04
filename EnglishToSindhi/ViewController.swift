4c6c6611a842eeb9bd2f8693a47b88c4e1872edd
Ayaz <ayazakbar24@gmail.com>
2021-05-04 11:01:58 +0500

    Initial Commit

---
+++ EnglishToSindhi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EnglishToSindhi
+//
+//  Created by ayaz akbar on 04/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
